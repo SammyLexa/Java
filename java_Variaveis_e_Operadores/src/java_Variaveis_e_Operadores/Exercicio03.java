12583aa2cc52e62ea66271749cfde20b580815c2
Samantha Alexandra <lexalaska31@gmail.com>
2023-02-08 17:04:01 -0300

    Atividade01

--- java_Variaveis_e_Operadores/src/java_Variaveis_e_Operadores/Exercicio03.java
+++ java_Variaveis_e_Operadores/src/java_Variaveis_e_Operadores/Exercicio03.java
@@ -9,10 +9,23 @@ public class Exercicio03 {
 	
 		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
 		
-		salarioBruto = 2000;
+		/*salarioBruto = 2000;
 		adicionalNoturno = 500;
 		horasExtras = 100;
-		descontos = 200;
+		descontos = 200*/
+		
+		System.out.print("Digite o salário bruto: ");
+		salarioBruto = leia.nextFloat();
+		
+		System.out.print("Digite o adicional noturno: ");
+		adicionalNoturno = leia.nextFloat();
+		
+		System.out.print("Digite as horas extras: ");
+		horasExtras = leia.nextFloat();
+		
+		System.out.print("Digite os descontos: ");
+		descontos = leia.nextFloat();
+		
 		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras*5) - descontos;
 		
 		System.out.print("O salário líquido é:" + salarioLiquido);
--- java_Variaveis_e_Operadores/src/java_Variaveis_e_Operadores/Exercicio04.java
+++ java_Variaveis_e_Operadores/src/java_Variaveis_e_Operadores/Exercicio04.java
@@ -7,16 +7,23 @@ public class Exercicio04 {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		
-		float n01, n02, n03, n3, n04, calculo, calculo2;
+		float n01, n02, n03, n04, calculo;
 		
-		n01 = 5;
+		/*n01 = 5;
 		n02 = 6;
-		n03 = 7;
-		n3 = -7;
-		n04 = 8;
+		n03 = 7 e -7;
+		n04 = 8;*/
+		
+		System.out.print("Digite o número 1: ");
+		n01 = leia.nextFloat();
+		System.out.print("Digite o número 2: ");
+		n02 = leia.nextFloat();
+		System.out.print("Digite o número 3: ");
+		n03 = leia.nextFloat();
+		System.out.print("Digite o número 4: ");
+		n04 = leia.nextFloat();
 		calculo = (n01 * n02) - (n03 * n04);
-		calculo2 = (n01 * n02) - (n3 * n04);
+		
 		System.out.println("A diferença é:" + calculo);
-		System.out.print("A diferença é:" + calculo2);
 	}
 }
