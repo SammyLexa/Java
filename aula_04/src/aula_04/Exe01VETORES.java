70c8c3f5cbb975b3cb929775c03cbbd17675e7ac
Samantha Alexandra <lexalaska31@gmail.com>
2023-02-13 22:42:26 -0300

    Matrizes e Vetores

--- aula_04/src/aula_04/Exe01VETORES.java
+++ aula_04/src/aula_04/Exe01VETORES.java
@@ -1,5 +1,32 @@
 package aula_04;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exe01VETORES {
+
+	static Scanner leia = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int Vetor[] = {2,5,1,3,4,9,7,8,10,6};
+		int n;
+		
+		System.out.println("Digite o número que deseja encontrar: ");
+		n = leia.nextInt();
+		
+		for(int c = 0; c < Vetor.length; c++) {
+			if(n == Vetor[c]) {
+				System.out.printf("O número %d está localicado na posição %d", n, c);
+				break;
+			}else {
+				if(Vetor.length - 1 == c) {
+					System.out.println("O número não foi achado...");
+				}
+				
+			}
+			
+		}
+		
+	}
 
 }
